768c7bb127c0d36027cf2863a697314458c585be
franciscosaurus <53538713+franciscosaurus@users.noreply.github.com>
2023-04-21 01:41:48 -0400

    added JSON and temp display and addProcess

--- database.py
+++ database.py
@@ -1,105 +1,24 @@
 # Imports
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from IPython.display import display
-import sqlite3
+import json
+from tabulate import tabulate
 
 # Global variables.
 size = 1000
 
-# Different DataFrames for each recycling process.
-
-# Fiber Materials
-# Mechanical Recycling - Beating
-df_beating_fiber = pd.DataFrame({'Material': ['Polyester', 'Polypropelene', 'Nylon 6', 'Nylon 66'], 
-                                 'Recyclability': [1, 2, 3, 4], 
-                                 'Cost': [1, 2, 3, 4], 
-                                 'Market Value': [1, 2, 3, 4]})
-
-
-# Mechanical Recycling - Shearing
-df_shearing_fiber = pd.DataFrame({'Material': ['Polyester', 'Polypropelene', 'Nylon 6', 'Nylon 66'], 
-                                  'Recyclability': [1, 2, 3, 4], 
-                                  'Cost': [1, 2, 3, 4], 
-                                  'Market Value': [1, 2, 3, 4]})
-
-# Mechanical Recycling - Separation
-df_rise_fiber = pd.DataFrame({'Material': ['Polyester', 'Polypropelene', 'Nylon 6', 'Nylon 66'], 
-                                    'Recyclability': [1, 2, 3, 4], 
-                                    'Cost': [1, 2, 3, 4], 
-                                    'Market Value': [1, 2, 3, 4]})
-
-# Chemical Recycling - Nylon Depolymerization
-df_ndpoly_fiber = pd.DataFrame({'Material': ['Polyester', 'Polypropelene', 'Nylon 6', 'Nylon 66'], 
-                                'Recyclability': [1, 2, 3, 4], 
-                                'Cost': [1, 2, 3, 4], 
-                                'Market Value': [1, 2, 3, 4]})
-
-# Chemical Recycling - Formic Acid Dissolution
-df_fadiss_fiber = pd.DataFrame({'Material': ['Polyester', 'Polypropelene', 'Nylon 6', 'Nylon 66'], 
-                                'Recyclability': [1, 2, 3, 4], 
-                                'Cost': [1, 2, 3, 4], 
-                                'Market Value': [1, 2, 3, 4]})
-
-# Backing Materials
-# Mechanical Recycling - Beating
-df_beating_backing = pd.DataFrame({'Material': ['Polyester', 'Polypropelene'], 
-                                   'Recyclability': [1, 2], 
-                                   'Cost': [1, 2], 
-                                   'Market Value': [1, 2]})
-
-# Mechanical Recycling - Shearing
-df_shearing_backing = pd.DataFrame({'Material': ['Polyester', 'Polypropelene'], 
-                                    'Recyclability': [1, 2], 
-                                    'Cost': [1, 2], 
-                                    'Market Value': [1, 2]})
-
-# Mechanical Recycling - Separation
-df_rise_backing = pd.DataFrame({'Material': ['Polyester', 'Polypropelene'], 
-                                      'Recyclability': [1, 2], 
-                                      'Cost': [1, 2], 
-                                      'Market Value': [1, 2]})
-
-# Chemical Recycling - Nylon Depolymerization
-df_ndpoly_backing = pd.DataFrame({'Material': ['Polyester', 'Polypropelene'], 
-                                  'Recyclability': [1, 2], 
-                                  'Cost': [1, 2], 
-                                  'Market Value': [1, 2]})
-
-# Chemical Recycling - Formic Acid Dissolution
-df_fadiss_backing = pd.DataFrame({'Material': ['Polyester', 'Polypropelene'], 
-                                  'Recyclability': [1, 2], 
-                                  'Cost': [1, 2], 
-                                  'Market Value': [1, 2]})
-
-beating = sqlite3.connect("beating.db")
-df_beating_fiber.to_sql("fiber", beating, if_exists="replace")
-df_beating_backing.to_sql("backing", beating, if_exists="replace")
-beating.execute()
-
-shearing = sqlite3.connect("shearing.db")
-df_shearing_fiber.to_sql("fiber", shearing, if_exists="replace")
-df_shearing_backing.to_sql("backing", shearing, if_exists="replace")
-shearing.execute()
-
-rise = sqlite3.connect("rise.db")
-df_rise_fiber.to_sql("fiber", rise, if_exists="replace")
-df_rise_backing.to_sql("backing", rise, if_exists="replace")
-rise.execute()
-
-ndpoly = sqlite3.connect("ndpoly.db")
-df_ndpoly_fiber.to_sql("fiber", ndpoly, if_exists="replace")
-df_ndpoly_backing.to_sql("backing", ndpoly, if_exists="replace")
-ndpoly.execute()
-
-fadiss = sqlite3.connect("fadiss.db")
-df_fadiss_fiber.to_sql("fiber", fadiss, if_exists="replace")
-df_fadiss_backing.to_sql("backing", fadiss, if_exists="replace")
-fadiss.execute()
-
-
 # Functions
+
+def display(name):
+    with open("Processes.json") as myFile:
+        fileData = json.load(myFile)
+        print(fileData)
+display("Process 1")
+
+def addProcess(ProcessName):
+    print("to be implemented")
+addProcess("Process X")
 
 def optimize(df, size):
     # Grab values from the input DataFrame.
@@ -141,4 +60,4 @@ def optimize(df, size):
 
     return [max_recycle, min_cost, amount_of_polyester, amount_of_polypropelene]
 
-beating_backing_optimized = optimize(df_beating_backing, size)
+#beating_backing_optimized = optimize(df_beating_backing, size)
